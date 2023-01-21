645af3ead735ab456802adf6370068ce36af31c6
Vraj0109 <vrajlimbachiya99@gmail.com>
2023-01-21 12:45:25 +0530

    added navbar to the project

--- src/componant/basic/Restorant.js
+++ src/componant/basic/Restorant.js
@@ -4,73 +4,27 @@ import MenuCard from "./menuCard";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
+import Container from "../../../node_modules/react-bootstrap/Container";
+import Nav from "../../../node_modules/react-bootstrap/Nav";
+import Navbar from "../../../node_modules/react-bootstrap/Navbar";
+import NavDropdown from "../../../node_modules/react-bootstrap/NavDropdown";
 const Restorant = () => {
   const [menuData, setMenuData] = useState(Menu);
   //const [MenuCard,setMenuData] = React.useState(Menu); one can also use this method to do use hooks
   return (
     <div className="row">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a class="nav-link" href="#">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">
-                Disabled
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto ms-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <MenuCard menuData={menuData} />
     </div>
   );
